350856cb4ef338334785992947fb4c5e7e0b2b6a
nachoetienotmoine <nachoetienotmoine@gmail.com>
2022-10-02 12:25:08 -0300

    proyecto dashboard - primer commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ¿y Karina?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          AK VAMO A SE EL DASBOR PAL ADMIN
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
