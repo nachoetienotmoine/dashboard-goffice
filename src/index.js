4b08aedf877b8ebcf3d1fe52ca7d34bc46236e0e
gonzaloandreozzi <andreozzigonzalo@gmail.com>
2022-10-11 03:47:48 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './assets/css/app.css';
+import './assets/css/App.css';
 
 // Import de stylesheets propios
 import './assets/css/styles.css'
