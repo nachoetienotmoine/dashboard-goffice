6795a16ffe794a0eef01ed109379688de8ecc677
unknown <nelson227@hotmail.es>
2022-10-14 07:06:59 -0300

    little bug fixed (repiting total de ventas)

--- src/components/Sold.js
+++ src/components/Sold.js
@@ -17,10 +17,10 @@ function Sold(props){
 
     return (
         <div>
+             <h1 className="col-lg-9">Total de ventas: {props.dataTotalExchanges}</h1>
             {dataUsers !== undefined && dataUsers.map((user,i) => {
                     return (
                         <React.Fragment key={i}>
-                            <h1 className="col-lg-9">Total de ventas: {props.dataTotalExchanges}</h1>
                             
                             <div className="col-lg-8 mb-4">
                             <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 10 +'rem'}} src={userImage.URL} alt = "imagen producto" />
